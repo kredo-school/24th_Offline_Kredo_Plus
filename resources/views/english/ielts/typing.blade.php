1e10eb00c5558bee3b575be9928834236bbfeca4
kazukitooyama1998-svg <kazukitooyama1998@gmail.com>
2026-08-17 18:17:59 +0900

    Changed "Quit Practice" button label to Japanese

--- resources/views/english/ielts/typing.blade.php
+++ resources/views/english/ielts/typing.blade.php
@@ -52,7 +52,7 @@ window.__TYPING_CONFIG__ = {
     <div class="mt-6 flex justify-end">
       <a href="{{ route('english.ielts.index') }}"
          class="px-6 py-2.5 bg-[#b95827] text-white font-bold rounded-[0.5rem] shadow-sm hover:bg-[#a04c22] transition-colors text-base">
-        Quit Practice
+        練習を中断する
       </a>
     </div>
   </div>
